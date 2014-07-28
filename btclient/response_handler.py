1bbd31866c2e801465e5dd46b86de5b9011f6fe9
Idris Raja <idris.raja@gmail.com>
2014-07-28 15:02:16 -0400

    redo directory structure

---
+++ btclient/response_handler.py
@@ -0,0 +1,57 @@
+from torrent_read import Torrent
+from bencode import bdecode
+import sys
+import requests
+
+
+class ResponseHandler(object):
+    def parse_response(self, response):
+        #TODO: figure out right way to handle this
+        if response.status_code != 200:
+            print 'invalid request'
+            sys.exit(1)
+        else:
+            for k, v in response.__dict__.items():
+                setattr(self, k, v)
+
+
+    def get_peer_ip(self):
+        self.peers = bdecode(self._content)['peers']
+
+        # expect 6 bytes per peer
+        assert len(list(self.peers)) % 6 == 0
+
+        #bs = bytearray(self.peers)
+        bs = [ord(b) for b in list(self.peers)]
+
+        n = 6
+        self.ip = []
+        for i in xrange(0, len(bs), n):
+            self.ip.append(tuple(bs[i:i+n]))
+
+
+    def __repr__(self):
+        print_fields = ['_content',
+                'ip',
+                'peers',
+                'status_code',
+                'url']
+
+        return '\n'.join(['%s:\t%r' % (field, getattr(self, field), ) for field in print_fields])
+
+
+if __name__ == '__main__':
+    try: 
+        filename = sys.argv[1]
+    except IndexError:
+        filename = '../torrent/flagfromserver.torrent'
+        #filename = '../torrent/linuxmint-16-cinnamon-dvd-64bit.torrent'
+
+    t = Torrent(filename)
+    t.bdecode()
+    t.make_request()
+    response = requests.get(t.request)
+    response_handler = ResponseHandler()
+    response_handler.parse_response(response)
+    response_handler.get_peer_ip()
+    r = response_handler
---
+++ btclient/wire.py
@@ -0,0 +1,33 @@
+'''
+class Wire(object):
+
+    def __init__(self):
+        am_choking
+
+
+class Peer(object):
+    def __init__(self):
+        self.am_choking = 1
+        self.am_interested = 0
+        self.peer_choking = 1
+        self.peer_interested = 0
+
+
+'''
+class Handshake(object):
+    def __init__(self):
+        self.pstr ='BitTorrent protocol'
+        self.pstrlen = chr(len(self.pstr))
+        self.reserved='\x00\x00\x00\x00\x00\x00\x00\x00'
+        self.info_hash='+\x15\xca+\xfdH\xcd\xd7m9\xecU\xa3\xab\x1b\x8aW\x18\n\t'
+        self.peer_id= 'aaaaaaaaaaaaaaaaaaaa'
+
+
+    def __str__(self):
+        fields = [self.pstrlen, self.pstr, self.reserved, self.info_hash, self.peer_id]
+        return ''.join(['%s' % field for field in fields])
+
+
+if __name__ == '__main__':
+    h = Handshake()
+    print repr(str(h))
