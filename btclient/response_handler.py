44d01eb852923dd29c08746644da1b6c44c0e5da
Idris Raja <idris.raja@gmail.com>
2014-07-28 16:04:22 -0400

    make ip handling easier

--- btclient/response_handler.py
+++ btclient/response_handler.py
@@ -5,6 +5,11 @@ import requests
 
 
 class ResponseHandler(object):
+    def __init__(self, response):
+        self.parse_response(response)
+        self.get_peer_ip()
+
+
     def parse_response(self, response):
         #TODO: figure out right way to handle this
         if response.status_code != 200:
@@ -25,14 +30,16 @@ class ResponseHandler(object):
         bs = [ord(b) for b in list(self.peers)]
 
         n = 6
-        self.ip = []
+        self.ips = []
         for i in xrange(0, len(bs), n):
-            self.ip.append(tuple(bs[i:i+n]))
+            ip = '.'.join([str(b) for b in bs[i:i+n-2]])
+            port = bs[i+(n-2)] * 256 + bs[i+(n-1)]
+            self.ips.append((ip, port))
 
 
     def __repr__(self):
         print_fields = ['_content',
-                'ip',
+                'ips',
                 'peers',
                 'status_code',
                 'url']
@@ -48,7 +55,6 @@ if __name__ == '__main__':
         #filename = '../torrent/linuxmint-16-cinnamon-dvd-64bit.torrent'
 
     t = Torrent(filename)
-    t.bdecode()
     t.make_request()
     response = requests.get(t.request)
     response_handler = ResponseHandler()
